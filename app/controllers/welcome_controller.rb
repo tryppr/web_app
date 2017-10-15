7a782a6c5817b13d41f74eb238fbe96c596fe6b3
Jie Lin Chen <jielinchen@Jies-MacBook-Pro.local>
2017-10-15 13:40:21 +0200

    added schedule

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,7 +14,7 @@ class WelcomeController < ApplicationController
   	@tryppr_route = TrypprRoute.new
   	@tryppr_route.start_location = '52.5078022,13.3889263'
   	@tryppr_route.end_location = '52.5078022,13.3889263'
-  	@tryppr_route.places = [Place.find(1), Place.find(4), Place.find(7), Place.find(10), Place.find(14), Place.find(16), Place.find(18)]
+  	@tryppr_route.places = [Place.find(1), Place.find(4), Place.find(7), Place.find(10), Place.find(14), Place.find(16), Place.find(18), Place.find(6)]
     @schedule = @tryppr_route.facilitate_places
     @route = @tryppr_route.puzzle_route
     render layout: 'application'
